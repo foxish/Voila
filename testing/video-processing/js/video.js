df9aeb316d726da93677beced08d28576f623e71
Foxish <anirudh4444@gmail.com>
2016-04-05 09:32:50 +0000

    added diff between image frames.. now computing it and finding section boundaries

--- testing/video-processing/js/video.js
+++ testing/video-processing/js/video.js
@@ -20,28 +20,57 @@ $(function() {
 
   video.addEventListener('seeked', function() {
       generateThumbnail(i);
-      i += 5;
+      i += 100;
       /// if we are not passed end, seek to next interval
       if (i <= video.duration) {
           video.currentTime = i;
       } else {
-          alert("done");
+          insertMap();
       }
   }, false);
   
-  
+  var prevImage = "";
   function generateThumbnail(i) {     
     ctx.drawImage(video, 0, 0);
     var dataURL = canvas.toDataURL();
-
+    compareImages(dataURL, prevImage);
+    prevImage = dataURL;
+    
     //create img
     var img = document.createElement('img');
     img.setAttribute('crossOrigin', 'anonymous');
     img.setAttribute('src', dataURL);
-
+    img.setAttribute('id', i + "sec");
+    
     //append img in container div
     document.getElementById('thumbnailContainer').appendChild(img);
   }
+  
+  function compareImages(imgD1, imgD2) {
+    if(imgD1 === "" || imgD2 === "") {
+      return;
+    }
+    
+    var api = resemble(imgD1).compareTo(imgD2).ignoreColors().onComplete(function(data){
+      // create diffy
+      var span = document.createElement('span');
+      span.innerHTML = data.misMatchPercentage;
+      document.getElementById('thumbnailContainer').appendChild(span);
+    });
+  }
+  
+  function insertMap(){
+    var canvas = jQuery("<canvas>", {id: "voila"});
+    $(canvas).drawRect({
+      fillStyle: 'red',
+      x: 0, y: 0,
+      width: jQuery('#video').width(),
+      height: 50
+    });
+    $(canvas).width = jQuery('#video').width();
+    jQuery("#video").after(canvas);
+  }
+  
 });
 
 
